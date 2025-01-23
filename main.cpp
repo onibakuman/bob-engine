1a2860ff0077c616d12ef37f5a5b588055b744a7
David Attid <david.attid@gmail.com>
2025-01-22 17:08:26 -0700

    properly did cmake

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main (int argc, char *argv[]) {
-
-    std::cout << "This is a test to see if the webhook works" << std::endl;
-    
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <SDL3/SDL.h>
+
+
+int main() {
+    std::cout << "PETER IS NOW BECOMING MASKU" << std::endl;
+
+    SDL_HasJoystick();
+
+    return 0;
+}
+
