689033b171f6b6e9f9d61f6d48d2f8680b8c78c3
David Attid <david.attid@gmail.com>
2025-01-22 22:53:42 -0700

    added debug symbols

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <SDL3/SDL.h>
+#include <vk_video/vulkan_video_codec_h265std.h>
 
 
 int main() {
     std::cout << "PETER IS NOW BECOMING MASKU" << std::endl;
 
     SDL_HasJoystick();
+
+    int test = vulkan_video_codec_h265std;
 
     return 0;
 }
