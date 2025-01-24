1832d449c37180bc72903af7eaf801bd863f183d
David Attid <david.attid@gmail.com>
2025-01-24 15:17:06 -0700

    registered events which will close the window and made it 1920 by 1080 more as a learning exercise

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -5,13 +7,13 @@
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
 #include <SDL3/SDL_timer.h>
+#include <SDL3/SDL_events.h>
     
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
 /* This function runs once at startup. */
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
-{
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Bob Game Engine - AKA Best of Bakuman", "1.0", "net.onibakuland.games");
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -19,7 +21,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 1920, 1920, 0, &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -28,17 +30,19 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 }
 
 /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
-{
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     if (event->type == SDL_EVENT_QUIT) {
         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
+    }
+
+    if(event->type == SDL_EVENT_KEY_DOWN) {
+        return SDL_APP_SUCCESS;
     }
     return SDL_APP_CONTINUE;  /* carry on with the program! */
 }
 
 /* This function runs once per frame, and is the heart of the program. */
-SDL_AppResult SDL_AppIterate(void *appstate)
-{
+SDL_AppResult SDL_AppIterate(void *appstate) {
     const double now = ((double)SDL_GetTicks()) / 1000.0;  /* convert from milliseconds to seconds. */
     /* choose the color for the frame we will draw. The sine wave trick makes it fade between colors smoothly. */
     const float red = (float) (0.5 + 0.5 * SDL_sin(now));
@@ -55,7 +59,7 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 }
 
 /* This function runs once at shutdown. */
-void SDL_AppQuit(void *appstate, SDL_AppResult result)
-{
-    /* SDL will clean up the window/renderer for us. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    SDL_Log("The program will shutdown now");
+    std::cout << "The program will shutdown now" << std::endl;
 }
